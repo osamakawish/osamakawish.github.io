58131addbb67d0f748abef8caf10e6d856e87c90
Osama Kawish <osamakawish@gmail.com>
2018-02-10 08:56:51 -0500

    Attempt to add navigation bar.

---
+++ index.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Osama Kawish - Home</title>
+</head>
+
+<style type="text/css">
+	#nav {
+		margin: 0;
+		padding: 0;
+	}
+	#nav li {
+		padding: 14px 0 14px 0;
+		list-style-type: none;
+		background-color: #222;
+		color: #eee;
+		width: 20%;
+		text-align: center;
+		margin: 0;
+		border: 0;
+	}
+	#nav {
+		width: 100%;
+	}
+	#nav li {
+		float: left;
+		display: inline-block;
+	}
+	#nav li:hover {
+		background-color: #eee;
+		color: #222;
+	}
+</style>
+
+<!-- Entire page is a navigation - Not necessarily. -->
+<body style="margin: 0; padding: 0;">
+	<!-- Main Navigation bar, covers entire laft side of main page. -->
+	<ul id="nav">
+		<!-- Probably want an image as part of each link. -->
+		<!-- Image should be a background that triggers on hover. -->
+		<!-- Also want to add a summary of navigation contents. -->
+		<li>Home
+			<ul>
+				
+			</ul>
+		</li>
+		<li>Writing</li>
+		<li>Code</li>
+		<li>Art</li>
+		<li>Feedback</li>
+	</ul>
+	
+	<!-- Navigation lists: right of page. Triggers when a certain navbar item is hovered over. --> 
+	<!-- Only turn on one of these at a time. -->
+	<div id="subnav">
+
+		<div id="Writing" class="nav1">
+			<ul>
+				<li>Poetry</li>
+				<li>Short Stories</li>
+				<li>Blog</li>
+			</ul>
+		</div>
+
+		<div id="Code" class="nav1">
+			<ul>
+				<li>Console Applications</li>
+				<li>Small Applications</li>
+				<li>Games</li>
+				<li>Github Profile</li>
+			</ul>
+		</div>
+
+		<div id="Art" class="nav1">
+			<ul>
+				<li>Sketches</li>
+				<li>Fashion Ideas</li>
+			</ul>
+		</div>
+
+		<div id="Feedback" class="nav1">
+			<ul>
+				<li>Contact Us</li>
+				<li>About Me</li>
+			</ul>
+		</div>
+
+	</div>
+
+
+</body>
+</html>
