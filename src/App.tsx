4dde0542821a5bba980a2f564a13602fe4aa102b
Osama Kawish <osamakawish@gmail.com>
2023-07-16 09:44:17 -0400

    Working full-page carousel example.

---
+++ src/App.tsx
@@ -0,0 +1,102 @@
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div
+        id="carousel"
+        className="carousel slide position-absolute w-100 h-100"
+      >
+        <div className="carousel-indicators">
+          <button
+            type="button"
+            data-bs-target="#carousel"
+            data-bs-slide-to="0"
+            className="active"
+            aria-current="true"
+            aria-label="Slide 1"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carousel"
+            data-bs-slide-to="1"
+            aria-label="Slide 2"
+          ></button>
+          <button
+            type="button"
+            data-bs-target="#carousel"
+            data-bs-slide-to="2"
+            aria-label="Slide 3"
+          ></button>
+        </div>
+        <div className="carousel-inner h-100 w-100">
+          <div className="carousel-item w-100 h-100 active">
+            <img
+              src="src/assets/lightning.jpg"
+              className="d-block object-fit-cover position-absolute h-100 w-100"
+              alt="..."
+            />
+            <div className="carousel-caption d-none d-md-block">
+              <h5>First slide label</h5>
+              <p>
+                Some representative placeholder content for the first slide.
+              </p>
+            </div>
+          </div>
+          <div className="carousel-item w-100 h-100">
+            <img
+              src="src/assets/arctic.jpg"
+              className="d-block object-fit-cover position-absolute h-100 w-100"
+              alt="..."
+            />
+            <div className="carousel-caption d-none d-md-block">
+              <h5>Second slide label</h5>
+              <p>
+                Some representative placeholder content for the second slide.
+              </p>
+            </div>
+          </div>
+          <div className="carousel-item w-100 h-100">
+            <img
+              src="src/assets/trees.jpg"
+              className="d-block object-fit-cover position-absolute h-100 w-100"
+              alt="..."
+            />
+            <div className="carousel-caption d-none d-md-block">
+              <h5>Third slide label</h5>
+              <p>
+                Some representative placeholder content for the third slide.
+              </p>
+            </div>
+          </div>
+        </div>
+        <button
+          className="carousel-control-prev"
+          type="button"
+          data-bs-target="#carousel"
+          data-bs-slide="prev"
+        >
+          <span
+            className="carousel-control-prev-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Previous</span>
+        </button>
+        <button
+          className="carousel-control-next"
+          type="button"
+          data-bs-target="#carousel"
+          data-bs-slide="next"
+        >
+          <span
+            className="carousel-control-next-icon"
+            aria-hidden="true"
+          ></span>
+          <span className="visually-hidden">Next</span>
+        </button>
+      </div>
+    </>
+  );
+}
+
+export default App;
