562c6f22e004cf4956f1ce36cc3b1697d47d356d
Osama Kawish <osamakawish@gmail.com>
2023-07-24 19:53:21 -0400

    Code improvements for flexibility.
    Remodified to be more flexible. Removed redundant images.

--- src/App.tsx
+++ src/App.tsx
@@ -1,44 +1,73 @@
-import "./App.css";
-import "./components/CarouselSlide";
+import { ReactNode, useEffect, useState } from "react";
 import CarouselSlide from "./components/CarouselSlide";
 import "./components/buttons/CarouselSlideButton";
 import LeftArrow from "./components/arrows/LeftArrow";
 import RightArrow from "./components/arrows/RightArrow";
 import SlideButtonGroup from "./components/buttons/SlideButtonGroup";
-import { useState } from "react";
+
+type CarouselSlideData = {
+  imgFile: string;
+  header: string;
+  description: ReactNode;
+};
 
 function App() {
   const [slideIndex, setSlideIndex] = useState(0);
+  const [isLoading, setIsLoading] = useState(true);
 
-  const slides = [
-    <CarouselSlide key={0} imgFile="Portfolio Intro.webp" header="Osama Kawish">
-      Mastering C#, Python, Typescript, HTML, CSS, and React, I deliver robust
-      software solutions. Explore to discover the elegance I bring to digital
-      complexity.
-    </CarouselSlide>,
-    <CarouselSlide
-      key={1}
-      imgFile="Animperium Preview.webp"
-      header="Animation Application"
-    >
-      These are the UI designs for an Animation application I'm currently
-      working on. The goal is to play around and create a simplified user
-      interface for animations. It is being implemented in Visual Studio via C#
-      and WPF.
-    </CarouselSlide>,
-    <CarouselSlide
-      key={2}
-      imgFile="Square API.webp"
-      header="Voice-Enabled Inventory Management"
-    >
-      Innovatively integrating C# with WPF and Azure's audio services, I've
-      built a voice-assistant application that efficiently manipulates inventory
-      through Square's API. Dive in to experience how I brought the future of
-      voice assistance to inventory management.
-    </CarouselSlide>,
+  const slidesInfo: Array<CarouselSlideData> = [
+    {
+      imgFile: "Portfolio Intro.webp",
+      header: "Osama Kawish",
+      description:
+        "Mastering C#, Python, Typescript, HTML, CSS, and React, I deliver robust software solutions. Explore to discover the elegance I bring to digital complexity.",
+    },
+    {
+      imgFile: "Animperium Preview.webp",
+      header: "Animation Application",
+      description:
+        "These are the UI designs for an Animation application I'm currently working on. The goal is to play around and create a simplified user interface for animations. It is being implemented in Visual Studio via C# and WPF.",
+    },
+    {
+      imgFile: "Square API.webp",
+      header: "Voice-Enabled Inventory Management",
+      description:
+        "Innovatively integrating C# with WPF and Azure's audio services, I've built a voice-assistant application that efficiently manipulates inventory through Square's API. Dive in to experience how I brought the future of voice assistance to inventory management.",
+    },
   ];
 
-  return (
+  // This effect runs once on mount and preloads all images
+  useEffect(() => {
+    const loadImages = async () => {
+      const promises = slidesInfo.map((slide) => {
+        return new Promise((resolve, reject) => {
+          const img = new Image();
+          img.src = slide.imgFile;
+          img.onload = resolve;
+          img.onerror = reject;
+        });
+      });
+
+      try {
+        await Promise.all(promises);
+        setIsLoading(false); // All images are loaded, we can stop showing the loading state
+      } catch (error) {
+        console.error("Failed to preload images", error);
+      }
+    };
+
+    loadImages();
+  }, [slidesInfo]);
+
+  const slides = slidesInfo.map((slide, index) => (
+    <CarouselSlide key={index} imgFile={slide.imgFile} header={slide.header}>
+      <p>{slide.description}</p>
+    </CarouselSlide>
+  ));
+
+  return isLoading ? (
+    <div>Loading...</div> // Or a more complex loading screen if you prefer
+  ) : (
     <>
       {slides[slideIndex]}
 
@@ -50,13 +79,11 @@ function App() {
 
       <LeftArrow
         onClick={() =>
-          setSlideIndex(Math.abs((slideIndex - 1) % slides.length))
+          setSlideIndex((slideIndex - 1 + slides.length) % slides.length)
         }
       />
       <RightArrow
-        onClick={() =>
-          setSlideIndex(Math.abs((slideIndex + 1) % slides.length))
-        }
+        onClick={() => setSlideIndex((slideIndex + 1) % slides.length)}
       />
     </>
   );
--- src/components/CarouselSlide.tsx
+++ src/components/CarouselSlide.tsx
@@ -1,19 +1,25 @@
+import { ReactNode, useEffect } from "react";
 import "./CarouselSlide.css";
 
 type CarouselImageProps = {
   imgFile: string;
   header?: string;
-  children?: string;
+  children?: ReactNode;
 };
 
 function CarouselSlide({ imgFile, header, children }: CarouselImageProps) {
+  useEffect(() => {
+    const img = new Image();
+    img.src = imgFile;
+  }, [imgFile]);
+
   return (
     <>
       <img src={imgFile} className="carousel-image" />
       <div className="rect" />
       <div className="text-block">
-        <h1 className="slide-header">{header}</h1>
-        <p className="text">{children}</p>
+        <h2 className="slide-header">{header}</h2>
+        {children}
       </div>
     </>
   );
