ad78f7cf867182aa2ebc40ec57ad31ff04578e5d
Osama Kawish <osamakawish@gmail.com>
2023-07-27 18:08:30 -0400

    Basic setup for content for the pages.

---
+++ src/components/about/AboutMe.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+function AboutMe() {
+  return (
+    <div>
+      <h1>This is the AboutMe page.</h1>
+      Probably a good idea to work on this next after finishing the off-canvas
+      on the home page.
+    </div>
+  );
+}
+
+export default AboutMe;
