8aeb88e4dadd5105c0527c027fc689df81762318
Osama Kawish <osamakawish@gmail.com>
2023-07-31 10:20:17 -0400

    Nlog preview modifications

--- src/components/blog/Blog.tsx
+++ src/components/blog/Blog.tsx
@@ -1,5 +1,4 @@
 // Blog.js
-import React from "react";
 import PartitionBlock from "./PartitionBlock";
 import "./Blog.css";
 import BlogPreview from "./BlogPreview";
--- src/components/blog/BlogPreview.tsx
+++ src/components/blog/BlogPreview.tsx
@@ -9,6 +9,13 @@ export default function BlogPreview({}: BlogPreviewProps) {
         <img className="blog-preview-img" src="./about-bg.webp" />
         <div className="blog-preview-content">
           <h4>Blog Preview</h4>
+          <p>
+            Pellentesque habitant morbi tristique senectus et netus et malesuada
+            fames ac turpis egestas. Vestibulum tortor quam, feugiat vitae,
+            ultricies eget, tempor sit amet, ante. Donec eu libero sit amet quam
+            egestas semper. Aenean ultricies mi vitae est. Mauris placerat
+            eleifend leo. Quisque sit amet est et sapien ullamcorper pharetra.
+          </p>
         </div>
       </div>
     </>
