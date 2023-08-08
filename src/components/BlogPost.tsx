eea6097e529bfbb19eddffab0e720c63fc05ec07
Osama Kawish <osamakawish@gmail.com>
2023-08-08 00:07:21 -0400

    Removed redundant import ReactNode.

--- src/components/BlogPost.tsx
+++ src/components/BlogPost.tsx
@@ -1,4 +1,4 @@
-import { ReactNode, useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import "./BlogPost.css";
 import { useParams } from "react-router-dom";
 import { blogPosts, GetDateFromId } from "../BlogPostData";
