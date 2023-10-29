fecb5d39fe9dfb3a3ac6d932b11d2ed14f4d79d4
Osama Kawish <osamakawish@gmail.com>
2023-10-29 17:45:16 -0400

    Basic site improvements.

--- src/components/pages/hire/HireMe.tsx
+++ src/components/pages/hire/HireMe.tsx
@@ -1,9 +1,10 @@
 export default function HireMe() {
   return (
     <>
-      <h1>Hire Me</h1>
+      <h1 className="page-header">Hire Me</h1>
       <div>
         <h2>Web Developer</h2>
+        <h2>Software Developer</h2>
         <h2>Graphic Designer</h2>
         <h2>Tutor</h2>
       </div>
