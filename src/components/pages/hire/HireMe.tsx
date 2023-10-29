0c216bc81c691877d10c68aa48ce422ea5687941
Osama Kawish <osamakawish@gmail.com>
2023-10-29 16:09:53 -0400

    HireMe basic content added.

--- src/components/pages/hire/HireMe.tsx
+++ src/components/pages/hire/HireMe.tsx
@@ -2,6 +2,11 @@ export default function HireMe() {
   return (
     <>
       <h1>Hire Me</h1>
+      <div>
+        <h2>Web Developer</h2>
+        <h2>Graphic Designer</h2>
+        <h2>Tutor</h2>
+      </div>
     </>
   );
 }
