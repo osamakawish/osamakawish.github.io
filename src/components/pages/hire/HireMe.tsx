8938ee28bf5aa3f311cee98ed8a5d10b1021e01a
Osama Kawish <osamakawish@gmail.com>
2023-11-04 12:16:47 -0400

    Implemeted forms. Needs style fixing on autofill.

--- src/components/pages/hire/HireMe.tsx
+++ src/components/pages/hire/HireMe.tsx
@@ -22,49 +22,68 @@ export default function HireMe() {
   }
 
   return (
-    <section id="hire-me-content">
-      <h1 className="page-header">Hire Me</h1>
+    <div id="hire-me-content">
+      <section>
+        <h1 className="page-header">Hire Me</h1>
 
-      <h1 style={{ color: "red" }}>
-        {jobType && convertCamelCaseToSpaced(jobType)}
-      </h1>
+        <h1 style={{ color: "red" }}>
+          {jobType && convertCamelCaseToSpaced(jobType)}
+        </h1>
 
-      <div id="jobTypeButtonGroup">
-        {jobTypes.map((_jobType, index) => {
-          return (
-            <>
-              <JobTypeButton
-                key={index}
-                jobType={_jobType}
-                isSelected={_jobType === jobType}
-                updateParams={(newJobType) =>
-                  updateParams("jobType", newJobType)
-                }
-              ></JobTypeButton>
-            </>
-          );
-        })}
-      </div>
-
-      <div id="priceTierGroup">
-        {jobType &&
-          priceTiers.map((_priceTier, index) => {
-            const priceInfo =
-              prices[jobType as JobType][_priceTier as PriceTier];
-
+        <div id="jobTypeButtonGroup">
+          {jobTypes.map((_jobType, index) => {
             return (
-              <PriceCard
-                key={index}
-                priceTier={_priceTier}
-                {...priceInfo}
-                selected={_priceTier === priceTier}
-                updateParams={(newPriceTier) =>
-                  updateParams("priceTier", newPriceTier)
-                }
-              />
+              <>
+                <JobTypeButton
+                  key={index}
+                  jobType={_jobType}
+                  isSelected={_jobType === jobType}
+                  updateParams={(newJobType) =>
+                    updateParams("jobType", newJobType)
+                  }
+                ></JobTypeButton>
+              </>
             );
           })}
-      </div>
-    </section>
+        </div>
+
+        <div id="priceTierGroup">
+          {jobType &&
+            priceTiers.map((_priceTier, index) => {
+              const priceInfo =
+                prices[jobType as JobType][_priceTier as PriceTier];
+
+              return (
+                <PriceCard
+                  key={index}
+                  priceTier={_priceTier}
+                  {...priceInfo}
+                  selected={_priceTier === priceTier}
+                  updateParams={(newPriceTier) =>
+                    updateParams("priceTier", newPriceTier)
+                  }
+                />
+              );
+            })}
+        </div>
+      </section>
+      <form
+        name="contact"
+        data-netlify
+        netlify-honeypot="bot-field"
+        id="contact-form"
+      >
+        <h5>Name</h5>
+        <input type="text" name="name" />
+        <h5>Email</h5>
+        <input type="email" name="email" />
+        <h5>Phone</h5>
+        <input type="tel" name="tel" />
+        <h5>Message</h5>
+        <textarea name="message" />
+        <button type="submit">Send</button>
+      </form>
+      <div className="spacer-xl"></div>
+    </div>
   );
 }
