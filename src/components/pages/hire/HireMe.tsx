5e4b47cc1a7af582ad003319b759c27544b8b7fb
Osama Kawish <osamakawish@gmail.com>
2023-11-08 22:48:35 -0500

    Submission bug fix attempt 3

--- src/components/pages/hire/HireMe.tsx
+++ src/components/pages/hire/HireMe.tsx
@@ -127,7 +127,7 @@ export default function HireMe() {
         )}
       </section>
 
-      <form netlify-honeypot="bot-field" id="contact-form" data-netlify>
+      <form netlify-honeypot="bot-field" id="contact-form" data-netlify="true">
         <input type="hidden" name="form-name" value="contact" />
         <input type="hidden" name="jobType" value={jobType || ""} />
         <input type="hidden" name="priceTier" value={priceTier || ""} />
