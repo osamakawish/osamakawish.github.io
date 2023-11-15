9d6ea1c06cc5cbdb5dadf4c9ba4233bd6d12d42a
Osama Kawish <osamakawish@gmail.com>
2023-11-15 14:29:03 -0500

    Basic code for light/dark mode prepared.

--- src/utilities/light-dark-mode.tsx
+++ src/utilities/light-dark-mode.tsx
@@ -1,1 +1,13 @@
+type ColorMode = "light" | "dark" | "system";
 
+export function setColorScheme(colorMode: ColorMode) {
+  const root = document.documentElement;
+
+  if (colorMode === "system") {
+    colorMode = window.matchMedia("(prefers-color-scheme: dark)").matches
+      ? "dark"
+      : "light";
+  }
+
+  root.setAttribute("color-mode", colorMode);
+}
